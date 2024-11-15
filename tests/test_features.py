7c60ad276a1b0b8d322ed2cf9aef3005ca8a9306
Gio-Formichella <gioformichella@gmail.com>
2024-11-15 23:08:41 +0100

    king_safety unittest

--- tests/test_features.py
+++ tests/test_features.py
@@ -6,7 +6,6 @@ class TestFeatures(unittest.TestCase):
 
     def test_piece_score(self):
         b = Board()
-        b.color_coords["KING"] = {(4, 4)}
         b.color_coords["WHITE"] = {
             (4, 2),
             (4, 3),
@@ -44,6 +43,79 @@ class TestFeatures(unittest.TestCase):
         # Unbalanced state
         self.assertEqual(piece_score(b, "WHITE"), -0.5)
         self.assertEqual(piece_score(b, "BLACK"), 0.5)
+
+    def test_king_safety(self):
+        b = Board()
+        b.color_coords["KING"] = {(4, 4)}
+
+        # King on throne, no close blacks
+        self.assertEqual(king_safety(b, "WHITE"), 1)
+        self.assertEqual(king_safety(b, "BLACK"), -1)
+
+        b.color_coords["BLACK"] = {(4, 3)}
+        b.coords_color[(4, 3)] = "BLACK"
+        # King on throne, 1 close black
+        self.assertEqual(king_safety(b, "WHITE"), 0.5)
+        self.assertEqual(king_safety(b, "BLACK"), -0.5)
+
+        b.color_coords["BLACK"] = {(4, 3), (4, 5)}
+        b.coords_color[(4, 5)] = "BLACK"
+        # King on throne, 2 close blacks
+        self.assertEqual(king_safety(b, "WHITE"), 0)
+        self.assertEqual(king_safety(b, "BLACK"), 0)
+
+        b.color_coords["BLACK"] = {(4, 3), (4, 5), (3, 4)}
+        b.coords_color[(3, 4)] = "BLACK"
+        # King on throne, 3 close blacks
+        self.assertEqual(king_safety(b, "WHITE"), -0.5)
+        self.assertEqual(king_safety(b, "BLACK"), 0.5)
+
+        b.color_coords["KING"] = {(5, 4)}
+        # King next to throne, no close blacks
+        self.assertEqual(king_safety(b, "WHITE"), 0.5)
+        self.assertEqual(king_safety(b, "BLACK"), -0.5)
+
+        b.color_coords["BLACK"] = {(4, 3), (4, 5), (3, 4), (5, 3)}
+        b.coords_color[(5, 3)] = "BLACK"
+        # King next to throne, 1 close black
+        self.assertEqual(king_safety(b, "WHITE"), 0)
+        self.assertEqual(king_safety(b, "BLACK"), 0)
+
+        b.color_coords["BLACK"] = {(4, 3), (4, 5), (3, 4), (5, 3), (5, 5)}
+        b.coords_color[(5, 5)] = "BLACK"
+        # King next to throne, 2 close blacks
+        self.assertEqual(king_safety(b, "WHITE"), -0.5)
+        self.assertEqual(king_safety(b, "BLACK"), 0.5)
+
+        b.color_coords["KING"] = {(7, 7)}
+        # King in the open, no close blacks
+        self.assertEqual(king_safety(b, "WHITE"), 0)
+        self.assertEqual(king_safety(b, "BLACK"), 0)
+
+        b.color_coords["KING"] = {(5, 6)}
+        # King in the open, 1 close black
+        self.assertEqual(king_safety(b, "WHITE"), -0.5)
+        self.assertEqual(king_safety(b, "BLACK"), 0.5)
+
+        b.color_coords["KING"] = {(4, 6)}
+        # King in the open, 1 close black and 1 citadel
+        self.assertEqual(king_safety(b, "WHITE"), -1)
+        self.assertEqual(king_safety(b, "BLACK"), 1)
+
+        b.color_coords["KING"] = {(3, 3)}
+        # King in the open, 2 close blacks but no capture
+        self.assertEqual(king_safety(b, "WHITE"), -1)
+        self.assertEqual(king_safety(b, "BLACK"), 1)
+
+        b.color_coords["KING"] = {(6, 4)}
+        # King in the open, 1 citadel
+        self.assertEqual(king_safety(b, "WHITE"), -0.5)
+        self.assertEqual(king_safety(b, "BLACK"), 0.5)
+
+        b.color_coords["KING"] = {(7, 5)}
+        # King in the open, 2 citadels
+        self.assertEqual(king_safety(b, "WHITE"), -1)
+        self.assertEqual(king_safety(b, "BLACK"), 1)
 
 
 if __name__ == "main":
