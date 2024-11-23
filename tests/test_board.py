57426a756014379f0ea5a7d9e795f42ad780253d
Gio-Formichella <gioformichella@gmail.com>
2024-11-23 12:54:04 +0100

    Changed strings to enum

--- tests/test_board.py
+++ tests/test_board.py
@@ -1,12 +1,12 @@
 import unittest
-from board import Board
+from board import *
 
 
 class TestBoard(unittest.TestCase):
     def test_get_available_moves(self):
         # Initial state
         b = Board()
-        b.color_coords["WHITE"] = {
+        b.color_coords[Color.WHITE] = {
             (4, 2),
             (4, 3),
             (2, 4),
@@ -16,9 +16,9 @@ class TestBoard(unittest.TestCase):
             (4, 5),
             (4, 6),
         }
-        for c in b.color_coords["WHITE"]:
-            b.coords_color[c] = "WHITE"
-        b.color_coords["BLACK"] = {
+        for c in b.color_coords[Color.WHITE]:
+            b.coords_color[c] = Color.WHITE
+        b.color_coords[Color.BLACK] = {
             (3, 0),
             (4, 0),
             (5, 0),
@@ -36,18 +36,18 @@ class TestBoard(unittest.TestCase):
             (4, 8),
             (5, 8),
         }
-        for c in b.color_coords["BLACK"]:
-            b.coords_color[c] = "BLACK"
-        b.color_coords["KING"] = {(4, 4)}
-        b.coords_color[(4, 4)] = "KING"
+        for c in b.color_coords[Color.BLACK]:
+            b.coords_color[c] = Color.BLACK
+        b.color_coords[Color.KING] = {(4, 4)}
+        b.coords_color[(4, 4)] = Color.KING
 
-        moves_white = b.get_available_moves("WHITE")
+        moves_white = b.get_available_moves(Color.WHITE)
         self.assertEqual(len(moves_white), 56)
         self.assertEqual(
             len(set(moves_white)), len(moves_white)
         )  # Duplicate move check
 
-        moves_black = b.get_available_moves("BLACK")
+        moves_black = b.get_available_moves(Color.BLACK)
         self.assertEqual(len(moves_black), 80)
         self.assertEqual(
             len(set(moves_white)), len(moves_white)
@@ -55,17 +55,17 @@ class TestBoard(unittest.TestCase):
 
         # Checking killer moves
         b = Board()
-        b.color_coords["KING"] = {(2, 4)}
-        b.coords_color[(2, 4)] = "KING"
-        b.color_coords["BLACK"] = {(2, 3), (1, 5)}
-        b.coords_color[(2, 3)] = "BLACK"
-        b.coords_color[(1, 5)] = "BLACK"
+        b.color_coords[Color.KING] = {(2, 4)}
+        b.coords_color[(2, 4)] = Color.KING
+        b.color_coords[Color.BLACK] = {(2, 3), (1, 5)}
+        b.coords_color[(2, 3)] = Color.BLACK
+        b.coords_color[(1, 5)] = Color.BLACK
 
-        moves_white = b.get_available_moves("WHITE")
+        moves_white = b.get_available_moves(Color.WHITE)
         self.assertTrue((2, 4, 2, 8) in moves_white)  # king escape
         self.assertTrue((2, 4, 3, 4) in moves_white)  # king moves to empty row
         self.assertEqual(len(moves_white), 5)
 
-        moves_black = b.get_available_moves("BLACK")
+        moves_black = b.get_available_moves(Color.BLACK)
         self.assertTrue((1, 5, 2, 5) in moves_black)  # black captures king
         self.assertEqual(len(moves_black), 18)
--- tests/test_features.py
+++ tests/test_features.py
@@ -6,7 +6,7 @@ class TestFeatures(unittest.TestCase):
 
     def test_piece_score(self):
         b = Board()
-        b.color_coords["WHITE"] = {
+        b.color_coords[Color.WHITE] = {
             (4, 2),
             (4, 3),
             (2, 4),
@@ -16,7 +16,7 @@ class TestFeatures(unittest.TestCase):
             (4, 5),
             (4, 6),
         }
-        b.color_coords["BLACK"] = {
+        b.color_coords[Color.BLACK] = {
             (3, 0),
             (4, 0),
             (5, 0),
@@ -36,128 +36,130 @@ class TestFeatures(unittest.TestCase):
         }
 
         # Even state
-        self.assertEqual(piece_score(b, "WHITE"), 0)
-        self.assertEqual(piece_score(b, "BLACK"), 0)
+        self.assertEqual(piece_score(b, Color.WHITE), 0)
+        self.assertEqual(piece_score(b, Color.BLACK), 0)
 
-        b.color_coords["WHITE"] = {(4, 2), (4, 3), (2, 4), (2, 3)}
+        b.color_coords[Color.WHITE] = {(4, 2), (4, 3), (2, 4), (2, 3)}
         # Unbalanced state
-        self.assertEqual(piece_score(b, "WHITE"), -0.5)
-        self.assertEqual(piece_score(b, "BLACK"), 0.5)
+        self.assertEqual(piece_score(b, Color.WHITE), -0.5)
+        self.assertEqual(piece_score(b, Color.BLACK), 0.5)
 
     def test_king_safety(self):
         b = Board()
-        b.color_coords["KING"] = {(4, 4)}
+        b.color_coords[Color.KING] = {(4, 4)}
 
         # King on throne, no close blacks
-        self.assertEqual(king_safety(b, "WHITE"), 1)
-        self.assertEqual(king_safety(b, "BLACK"), -1)
+        self.assertEqual(king_safety(b, Color.WHITE), 1)
+        self.assertEqual(king_safety(b, Color.BLACK), -1)
 
-        b.color_coords["BLACK"] = {(4, 3)}
-        b.coords_color[(4, 3)] = "BLACK"
+        b.color_coords[Color.BLACK] = {(4, 3)}
+        b.coords_color[(4, 3)] = Color.BLACK
         # King on throne, 1 close black
-        self.assertEqual(king_safety(b, "WHITE"), 0.5)
-        self.assertEqual(king_safety(b, "BLACK"), -0.5)
+        self.assertEqual(king_safety(b, Color.WHITE), 0.5)
+        self.assertEqual(king_safety(b, Color.BLACK), -0.5)
 
-        b.color_coords["BLACK"] = {(4, 3), (4, 5)}
-        b.coords_color[(4, 5)] = "BLACK"
+        b.color_coords[Color.BLACK] = {(4, 3), (4, 5)}
+        b.coords_color[(4, 5)] = Color.BLACK
         # King on throne, 2 close blacks
-        self.assertEqual(king_safety(b, "WHITE"), 0)
-        self.assertEqual(king_safety(b, "BLACK"), 0)
+        self.assertEqual(king_safety(b, Color.WHITE), 0)
+        self.assertEqual(king_safety(b, Color.BLACK), 0)
 
-        b.color_coords["BLACK"] = {(4, 3), (4, 5), (3, 4)}
-        b.coords_color[(3, 4)] = "BLACK"
+        b.color_coords[Color.BLACK] = {(4, 3), (4, 5), (3, 4)}
+        b.coords_color[(3, 4)] = Color.BLACK
         # King on throne, 3 close blacks
-        self.assertEqual(king_safety(b, "WHITE"), -0.5)
-        self.assertEqual(king_safety(b, "BLACK"), 0.5)
+        self.assertEqual(king_safety(b, Color.WHITE), -0.5)
+        self.assertEqual(king_safety(b, Color.BLACK), 0.5)
 
-        b.color_coords["KING"] = {(5, 4)}
+        b.color_coords[Color.KING] = {(5, 4)}
         # King next to throne, no close blacks
-        self.assertEqual(king_safety(b, "WHITE"), 0.5)
-        self.assertEqual(king_safety(b, "BLACK"), -0.5)
+        self.assertEqual(king_safety(b, Color.WHITE), 0.5)
+        self.assertEqual(king_safety(b, Color.BLACK), -0.5)
 
-        b.color_coords["BLACK"] = {(4, 3), (4, 5), (3, 4), (5, 3)}
-        b.coords_color[(5, 3)] = "BLACK"
+        b.color_coords[Color.BLACK] = {(4, 3), (4, 5), (3, 4), (5, 3)}
+        b.coords_color[(5, 3)] = Color.BLACK
         # King next to throne, 1 close black
-        self.assertEqual(king_safety(b, "WHITE"), 0)
-        self.assertEqual(king_safety(b, "BLACK"), 0)
+        self.assertEqual(king_safety(b, Color.WHITE), 0)
+        self.assertEqual(king_safety(b, Color.BLACK), 0)
 
-        b.color_coords["BLACK"] = {(4, 3), (4, 5), (3, 4), (5, 3), (5, 5)}
-        b.coords_color[(5, 5)] = "BLACK"
+        b.color_coords[Color.BLACK] = {(4, 3), (4, 5), (3, 4), (5, 3), (5, 5)}
+        b.coords_color[(5, 5)] = Color.BLACK
         # King next to throne, 2 close blacks
-        self.assertEqual(king_safety(b, "WHITE"), -0.5)
-        self.assertEqual(king_safety(b, "BLACK"), 0.5)
+        self.assertEqual(king_safety(b, Color.WHITE), -0.5)
+        self.assertEqual(king_safety(b, Color.BLACK), 0.5)
 
-        b.color_coords["KING"] = {(7, 7)}
+        b.color_coords[Color.KING] = {(7, 7)}
         # King in the open, no close blacks
-        self.assertEqual(king_safety(b, "WHITE"), 0)
-        self.assertEqual(king_safety(b, "BLACK"), 0)
+        self.assertEqual(king_safety(b, Color.WHITE), 0)
+        self.assertEqual(king_safety(b, Color.BLACK), 0)
 
-        b.color_coords["KING"] = {(5, 6)}
+        b.color_coords[Color.KING] = {(5, 6)}
         # King in the open, 1 close black
-        self.assertEqual(king_safety(b, "WHITE"), -0.5)
-        self.assertEqual(king_safety(b, "BLACK"), 0.5)
+        self.assertEqual(king_safety(b, Color.WHITE), -0.5)
+        self.assertEqual(king_safety(b, Color.BLACK), 0.5)
 
-        b.color_coords["KING"] = {(4, 6)}
+        b.color_coords[Color.KING] = {(4, 6)}
         # King in the open, 1 close black and 1 citadel
-        self.assertEqual(king_safety(b, "WHITE"), -1)
-        self.assertEqual(king_safety(b, "BLACK"), 1)
+        self.assertEqual(king_safety(b, Color.WHITE), -1)
+        self.assertEqual(king_safety(b, Color.BLACK), 1)
 
-        b.color_coords["KING"] = {(3, 3)}
+        b.color_coords[Color.KING] = {(3, 3)}
         # King in the open, 2 close blacks but no capture
-        self.assertEqual(king_safety(b, "WHITE"), -1)
-        self.assertEqual(king_safety(b, "BLACK"), 1)
+        self.assertEqual(king_safety(b, Color.WHITE), -1)
+        self.assertEqual(king_safety(b, Color.BLACK), 1)
 
-        b.color_coords["KING"] = {(6, 4)}
+        b.color_coords[Color.KING] = {(6, 4)}
         # King in the open, 1 citadel
-        self.assertEqual(king_safety(b, "WHITE"), -0.5)
-        self.assertEqual(king_safety(b, "BLACK"), 0.5)
+        self.assertEqual(king_safety(b, Color.WHITE), -0.5)
+        self.assertEqual(king_safety(b, Color.BLACK), 0.5)
 
-        b.color_coords["KING"] = {(7, 5)}
+        b.color_coords[Color.KING] = {(7, 5)}
         # King in the open, 2 citadels
-        self.assertEqual(king_safety(b, "WHITE"), -1)
-        self.assertEqual(king_safety(b, "BLACK"), 1)
+        self.assertEqual(king_safety(b, Color.WHITE), -1)
+        self.assertEqual(king_safety(b, Color.BLACK), 1)
 
     def test_capture_king(self):
         b = Board()
-        self.assertEqual(capture_king(b, "WHITE"), float("-inf"))
-        self.assertEqual(capture_king(b, "BLACK"), float("inf"))
+        self.assertEqual(capture_king(b, Color.WHITE), float("-inf"))
+        self.assertEqual(capture_king(b, Color.BLACK), float("inf"))
 
+    def test_king_free_road(self):
+        b = Board()
+        b.color_coords[Color.KING] = {(4, 2)}
+        # King in empty col
+        self.assertEqual(king_free_road(b, Color.WHITE), 100)
+        self.assertEqual(king_free_road(b, Color.BLACK), -100)
+
+        b.color_coords[Color.KING] = {(2, 4)}
+        # King in empty row
+        self.assertEqual(king_free_road(b, Color.WHITE), 100)
+        self.assertEqual(king_free_road(b, Color.BLACK), -100)
+        
     def test_win_move_king(self):
         b = Board()
-        b.color_coords["KING"] = {(4, 2)}
-        # King in empty col
-        self.assertEqual(win_move_king(b, "WHITE"), float("inf"))
-        self.assertEqual(win_move_king(b, "BLACK"), float("-inf"))
-
-        b.color_coords["KING"] = {(2, 4)}
-        # King in empty row
-        self.assertEqual(win_move_king(b, "WHITE"), float("inf"))
-        self.assertEqual(win_move_king(b, "BLACK"), float("-inf"))
-
         # King on boarder
-        b.color_coords["KING"] = {(3, 0)}
-        self.assertEqual(win_move_king(b, "WHITE"), float("inf"))
-        self.assertEqual(win_move_king(b, "BLACK"), float("-inf"))
+        b.color_coords[Color.KING] = {(3, 0)}
+        self.assertEqual(win_move_king(b, Color.WHITE), float("inf"))
+        self.assertEqual(win_move_king(b, Color.BLACK), float("-inf"))
 
-        b.color_coords["KING"] = {(0, 5)}
-        self.assertEqual(win_move_king(b, "WHITE"), float("inf"))
-        self.assertEqual(win_move_king(b, "BLACK"), float("-inf"))
+        b.color_coords[Color.KING] = {(0, 5)}
+        self.assertEqual(win_move_king(b, Color.WHITE), float("inf"))
+        self.assertEqual(win_move_king(b, Color.BLACK), float("-inf"))
 
     def test_king_distance(self):
         b = Board()
-        b.color_coords["KING"] = {(3, 3)}
-        b.color_coords["BLACK"] = {(6, 6)}
-        b.coords_color[(6, 6)] = "BLACK"
+        b.color_coords[Color.KING] = {(3, 3)}
+        b.color_coords[Color.BLACK] = {(6, 6)}
+        b.coords_color[(6, 6)] = Color.BLACK
         # Free quadrant
-        self.assertEqual(king_distance(b, "WHITE"), -((1.5 - 0.25) / 7.25 * 2 - 1))
-        self.assertEqual(king_distance(b, "BLACK"), ((1.5 - 0.25) / 7.25 * 2 - 1))
+        self.assertEqual(king_distance(b, Color.WHITE), -((1.5 - 0.25) / 7.25 * 2 - 1))
+        self.assertEqual(king_distance(b, Color.BLACK), ((1.5 - 0.25) / 7.25 * 2 - 1))
 
-        b.color_coords["BLACK"] = {(6, 6), (2, 1), (1, 2)}
-        b.coords_color[(2, 1)] = "BLACK"
-        b.coords_color[(1, 2)] = "BLACK"
+        b.color_coords[Color.BLACK] = {(6, 6), (2, 1), (1, 2)}
+        b.coords_color[(2, 1)] = Color.BLACK
+        b.coords_color[(1, 2)] = Color.BLACK
         # 1 black in quadrant with king
-        self.assertEqual(king_distance(b, "WHITE"), -((2 - 0.25) / 7.25 * 2 - 1))
-        self.assertEqual(king_distance(b, "BLACK"), ((2 - 0.25) / 7.25 * 2 - 1))
+        self.assertEqual(king_distance(b, Color.WHITE), -((2 - 0.25) / 7.25 * 2 - 1))
+        self.assertEqual(king_distance(b, Color.BLACK), ((2 - 0.25) / 7.25 * 2 - 1))
 
 
 if __name__ == "main":
