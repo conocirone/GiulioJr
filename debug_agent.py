6b66ae34a11be6a2e23eeb2cead75ef9b382c8e1
Gio-Formichella <gioformichella@gmail.com>
2024-11-25 14:42:50 +0100

    debug agent

---
+++ debug_agent.py
@@ -0,0 +1,164 @@
+import time
+import copy
+
+
+from board import Board, Color
+from features import (
+    piece_score,
+    king_safety,
+    king_distance,
+    king_free_road,
+)
+from state import State, Player
+
+
+class DebugAgent:
+    def __init__(self, timeout, color, board):
+        self.color = color
+        self.board = board
+        self.timeout = timeout
+        self.draw_fifo = []
+        
+        # Update draw_fifo
+        if len(self.draw_fifo) == 4:
+            self.draw_fifo.pop(0)
+        self.draw_fifo.append(self.board.color_coords)
+
+        # Define depth, timeout percentage
+        move = self.iterative_deepening(time.time() + self.timeout * 0.95)
+
+        conv_move = self.convert_move(move)
+        print(f"Chosen move: {conv_move}")
+
+    def convert_move(self, move):
+        """converts move indexes from integers to board format (letter-number)
+
+        Args:
+            move (tuple): (row_start, column_start, row_stop, column_stop)
+
+        Returns:
+            tuple: (starting_position, ending_position)
+        """
+        return chr(move[1] + 97) + str(move[0] + 1), chr(move[3] + 97) + str(
+            move[2] + 1
+        )
+
+    def iterative_deepening(self, time_limit):
+        depth = 2
+        best_move = None
+        max_depth = 100
+        while time.time() < time_limit and depth <= max_depth:
+
+            move, value = self.alphabeta_it(time_limit=time_limit, depth=depth)
+
+            if move is not None:
+                best_move = move
+                print(f"{self.color.name}: depth: {depth}, value: {value:.2f}, move: {self.convert_move(best_move)}")
+                depth += 1
+
+        if best_move is None:
+            best_move = self.board.get_available_moves(self.color)[0]
+
+        return best_move
+
+    def alphabeta_it(self, time_limit, depth):
+        root_state = State(
+            self.board,
+            None,
+            float("-inf"),
+            self.color,
+            Player.MAX,
+            float("-inf"),
+            float("inf"),
+            self.draw_fifo,
+            self.color
+        )
+        L = [root_state]
+
+        while not root_state.evaluated:
+
+            state = L[-1]
+
+            if state.evaluated:
+                del L[-1]
+                parent = L[-1]
+
+                if parent.player == Player.MAX:
+                    if state.value >= parent.value:
+                        parent.value = state.value
+                        parent.best_move = state.move
+                    parent.alpha = max(parent.alpha, parent.value)
+                    if parent.alpha >= parent.beta:
+                        Board.history_table[parent.color.value][parent.best_move] = (
+                            Board.history_table[parent.color.value].get(parent.best_move, 0)
+                            + 2 ** (depth - len(L) - 1)
+                        )
+                        parent.evaluated = True
+                        continue
+                else:
+                    if state.value <= parent.value:
+                        parent.value = state.value
+                        parent.best_move = state.move
+                    parent.beta = min(parent.beta, parent.value)
+                    if parent.alpha >= parent.beta:
+                        Board.history_table[parent.color.value][parent.best_move] = (
+                            Board.history_table[parent.color.value].get(parent.best_move, 0)
+                            + 2 ** (depth - len(L) - 1)
+                        )
+                        parent.evaluated = True
+                        continue
+
+                next_state = parent.next_state()
+                if next_state != parent:
+                    L.append(next_state)
+                else:
+                    Board.history_table[parent.color.value][parent.best_move] = (
+                        Board.history_table[parent.color.value].get(parent.best_move, 0)
+                        + 2 ** (depth - len(L) - 1)
+                    )
+                    parent.evaluated = True
+
+            elif len(L) == depth + 1:
+                state.value = self.eval(state.board)
+                state.evaluated = True
+
+            elif time.time() >= time_limit:
+                return None, None
+
+            else:
+                next_state = state.next_state()
+                if next_state != state:
+                    L.append(next_state)
+
+        return root_state.best_move, root_state.value
+
+    def eval(self, state):
+        kfr = king_free_road(state, self.color)
+        if kfr != 0:
+            return kfr
+
+        # Feature linear combination
+        s = 0
+        s += 0.3 * piece_score(state, self.color)
+        s += 0.1 * king_safety(state, self.color)
+        s += 0.6 * king_distance(state, self.color)
+        # other features
+        return s
+
+
+if __name__ == "__main__":
+    critical_state = [
+        ["EMPTY", "BLACK", "EMPTY", "EMPTY", "BLACK", "EMPTY", "EMPTY", "EMPTY", "EMPTY"],
+        ["EMPTY", "EMPTY", "BLACK", "EMPTY", "EMPTY", "BLACK", "EMPTY", "EMPTY", "EMPTY"],
+        ["EMPTY", "EMPTY", "EMPTY", "EMPTY", "WHITE", "EMPTY", "EMPTY", "BLACK", "EMPTY"],
+        ["BLACK", "EMPTY", "EMPTY", "EMPTY", "WHITE", "EMPTY", "EMPTY", "EMPTY", "EMPTY"],
+        ["BLACK", "EMPTY", "EMPTY", "BLACK", "KING", "BLACK", "EMPTY", "EMPTY", "EMPTY"],
+        ["EMPTY", "EMPTY", "EMPTY", "EMPTY", "EMPTY", "EMPTY", "EMPTY", "BLACK", "EMPTY"],
+        ["WHITE", "EMPTY", "BLACK", "BLACK", "EMPTY", "EMPTY", "WHITE", "EMPTY", "EMPTY"],
+        ["EMPTY", "EMPTY", "EMPTY", "EMPTY", "EMPTY", "EMPTY", "EMPTY", "EMPTY", "EMPTY"],
+        ["EMPTY", "EMPTY", "WHITE", "EMPTY", "BLACK", "EMPTY", "EMPTY", "EMPTY", "EMPTY"]
+    ]
+    
+    b = Board()
+    b.update(critical_state)
+    agent = DebugAgent(36000, color=Color.WHITE, board=b)
